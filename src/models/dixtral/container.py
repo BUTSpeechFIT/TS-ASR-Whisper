3ea141b94766162fa7fcbb0efa6c08a6625336d8
Alexander Polok <ipoloka@fit.vut.cz>
2026-01-21 05:50:33 +0100

    no CTC + lora

--- src/models/dixtral/container.py
+++ src/models/dixtral/container.py
@@ -46,8 +46,8 @@ class DixtralContainer:
 
             # Then override specific values
             config.audio_config.use_dicow_encoder = True
-            config.audio_config.ctc_weight = 0.2
-            config.audio_config.additional_layer = True
+            config.audio_config.ctc_weight = 0.0
+            config.audio_config.additional_layer = False
             config.audio_config.additional_self_attention_layer = False
             config.audio_config.pre_ctc_sub_sample = False
 
@@ -93,7 +93,7 @@ class DixtralContainer:
             lora_config = LoraConfig(
                 r=64,
                 lora_alpha=32,
-                target_modules=r".*language_model.*(q_proj|k_proj|v_proj|o_proj|down_proj|up_proj).*",
+                target_modules=r".*language_model.*(q_proj|k_proj|v_proj|o_proj|down_proj|up_proj|gate_proj).*",
                 lora_dropout=0.0,
                 bias="none",
             )
